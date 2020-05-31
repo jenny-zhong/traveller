3824e4e09bb502f70b78a0bdf5dbaa7651a476ee
Jenny <35668149+jenny-zhong@users.noreply.github.com>
2020-05-31 01:17:37 -0400

    update mocks

--- src/mocks/mockVisited.js
+++ src/mocks/mockVisited.js
@@ -1,30 +1,47 @@
-export const mockData = {
-        "Canada": {
-            "Toronto": {
-                "longitude": -79.3832,
-                "latitude": 43.6532
-            }
-        },
-        "America": {
-            "New York City": {
-                "longitude": -74.0060,
-                "latitude": 40.7128,
-            },
-            "San Francisco": {
-                "longitude": -122.4194,
-                "latitude": 37.7749,
-            }
-        },
-        "Japan": {
-            "Tokyo": {
-                "longitude": 139.6503,
-                "latitude": 35.6762, 
-            }
-        },
-        "China": {
-            "Beijing": {
-                "longitude": 116.4074,
-                "latitude":  39.9042,
-            }
-        }
-    }
+export const mockData = [
+	{
+		country: 'Canada',
+		cities: [
+			{
+				city: 'Toronto',
+				longitude: -79.3832,
+				latitude: 43.6532,
+			},
+		],
+	},
+	{
+		country: 'America',
+		cities: [
+			{
+				city: 'New York City',
+				longitude: -74.006,
+				latitude: 40.7128,
+			},
+			{
+				city: 'San Francisco',
+				longitude: -122.4194,
+				latitude: 37.7749,
+			},
+		],
+	},
+	{
+		country: 'Japan',
+		cities: [
+			{
+				city: 'Tokyo',
+				longitude: 139.6503,
+				latitude: 35.6762,
+			},
+		],
+	},
+	{
+		country: 'China',
+		cities: [
+			{
+				city: 'Beijing',
+				longitude: 116.4074,
+				latitude: 39.9042,
+			},
+		],
+	},
+]
