300e8a5289569423b12f7b7a4c223bc595d854a3
Jenny <35668149+jenny-zhong@users.noreply.github.com>
2020-07-14 01:16:49 -0400

    theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import { Map } from './components/Map/Map'
+import { List } from './components/List/List'
 
 const App = () => {
 	return (
 		<div className="App">
 			<Map />
+			<List />
 		</div>
 	)
 }
