1bcbe2bf936f69033d98679e11940d1dc70b5ac7
Jenny <35668149+jenny-zhong@users.noreply.github.com>
2020-09-01 23:26:22 -0400

    add removeCity to context

--- web/src/Context.ts
+++ web/src/Context.ts
@@ -9,19 +9,23 @@ type City = {
 type CityContext = {
 	cities: City[]
 	addCity: (city: City) => void
+	removeCity: (city: City) => void
 }
 
 export const CitiesContext = React.createContext<CityContext>({
 	cities: [],
 	addCity: () => {},
+	removeCity: () => {},
 })
 
 export const useCityContext = () => {
 	const [cities, setCities] = useState(visitedCities)
 	const addCity = (city: City) => setCities([...cities, city])
+	const removeCity = (city: City) => setCities(cities.filter((x) => x !== city))
 
 	return {
 		cities,
 		addCity,
+		removeCity,
 	}
 }
